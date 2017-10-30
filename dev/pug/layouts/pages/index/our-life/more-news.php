56dabfb76558b7c9e093fb4831e2a54f7eedfd19
Steven Gusev <stephen.gusev@gmail.com>
2017-10-30 07:29:50 +0700

    news page tweaks

--- dev/pug/layouts/pages/index/our-life/more-news.php
+++
@@ -1,1 +0,0 @@
-<a class="more-news" href="news.html">Еще новости</a>
--- dev/pug/layouts/pages/index/our-life/news-previews.php
+++ dev/pug/layouts/pages/index/our-life/news-previews.php
@@ -1,17 +1,24 @@
-<?php $i=0; if (have_posts()) : while (have_posts() && $i<3) : the_post(); ?>
+<?php
+	$args = array( 'numberposts' => '3' );
+	$recent_posts = wp_get_recent_posts( $args );
+	foreach( $recent_posts as $recent ){
+?>
 	<div class="news-preview">
 		<div class="image"<?php
-			if ( $thumbnail_id = get_post_thumbnail_id() ) {
+			if ( $thumbnail_id = get_post_thumbnail_id($recent["ID"]) ) {
 					if ( $image_src = wp_get_attachment_image_src( $thumbnail_id, 'normal-bg' ) )
 							printf( ' style="background-image: url(%s);"', $image_src[0] );
 			}
 		?>></div>
 		<div class="text">
-			<a href="<?php the_permalink(); ?>">
-				<h3><?php the_title(); ?></h3>
+			<a href="<?php echo get_post_type_archive_link( 'post' ); ?>">
+				<h3><?php echo $recent["post_title"]; ?></h3>
 			</a>
-			<time class="date"><?php echo get_the_date(); ?></time>
-			<p><?php the_excerpt(); ?></p>
+			<time class="date"><?php echo get_the_date('', $recent['ID']); ?></time>
+			<p><?php echo get_the_excerpt($recent['ID']); ?></p>
 		</div>
 	</div>
-<?php $i++; endwhile; endif; ?>
+<?php
+	};
+	wp_reset_query();
+?>
